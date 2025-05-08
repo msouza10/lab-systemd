809bd8e34e0a8562286926c727c0393a4a16d02f
Marcos Souza <marcos.souza101907@gmail.com>
2025-05-07 22:07:20 -0300

    adding new script C

--- lab/setup.sh
+++ lab/setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
+# Compilar o service e adicionando binario de exec.
+gcc lab-solver-full-updated.c -o /usr/local/bin/lab-solver-full
+chmod +x /usr/local/bin/lab-solver-full
+
 # Permite o systemd dentro do container
 exec /sbin/init
+systemctl daemon-reload
+systemctl enable --now faulty
 systemctl start stress
 systemctl start faulty
