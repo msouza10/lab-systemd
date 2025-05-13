9a3a44bb40f3581c0f4d59a899746478daa5ad97
Marcos Souza <135026379+msouza10@users.noreply.github.com>
2025-05-12 22:25:21 -0300

    Update setup.sh

--- lab/setup.sh
+++ lab/setup.sh
@@ -6,8 +6,12 @@ gcc /root/faulty.c -o /usr/local/bin/faulty
 chmod +x /usr/local/bin/faulty
 
 # Permite o systemd dentro do container
-exec /sbin/init
-systemctl daemon-reload
+exec /sbin/init &
+
+sleep
+
 systemctl enable --now faulty
 systemctl start stress.service
 systemctl start faulty.service
+
+systemctl daemon-reload
