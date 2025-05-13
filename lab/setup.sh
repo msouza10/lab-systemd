496e213d0a650bc8a31fb10d7d825822065702d4
Marcos <marcos.souza101907@gmail.com>
2025-05-13 03:45:51 +0200

    rollback setup.sh

--- lab/setup.sh
+++ lab/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
+# Compilar o service e adicionando binario de exec.
 gcc /root/faulty.c -o /usr/local/bin/faulty
 chmod +x /usr/local/bin/faulty
-/sbin/init &
-sleep 5
+
+# Permite o systemd dentro do container
+exec /sbin/init
 systemctl daemon-reload
-systemctl enable faulty.service
-systemctl enable stress.service
-systemctl start faulty.service
+systemctl enable --now faulty
 systemctl start stress.service
-tail -f /dev/null
+systemctl start faulty.service
